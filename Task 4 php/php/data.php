8759994eb3cc5746cbc9fe96462ebd2ebc2e1a0a
Amarnath k <amarnathk153@gmail.com>
2018-08-26 20:51:02 +0530

    small change

---
+++ Task 4 php/php/data.php
@@ -0,0 +1,77 @@
+<html>
+<title>data</title>
+<head><link rel="stylesheet" href="./css/bootstrap.min.css">
+	<meta http-equiv="refresh" content="4;URL='index.html'">
+	</head>
+<body>
+	<?php
+
+if($_COOKIE["user"]=="1"){
+	$conn->close();
+
+$cookie_name = "user";
+$cookie_value = "2";
+setcookie($cookie_name, $cookie_value, time() + 300, "/"); 
+
+$cookie_name = "id";
+$cookie_value = $last_id;
+setcookie($cookie_name, $cookie_value, time() + 3600, "/"); 
+
+}
+
+?>
+
+
+<!-- 
+
+close(); $cookie_name = "user"; $cookie_value = "2"; setcookie($cookie_name, $cookie_value, time() + 200, "/"); $cookie_name = "id"; $cookie_value = $last_id; setcookie($cookie_name, $cookie_value, time() + 3600, "/"); } ?>
+
+ -->
+
+
+
+	<div class="container">   
+	<fieldset>
+<!-- Form Name -->
+<legend>Join Us Today!</legend>
+	<div class="alert alert-success" role="alert" id="success_message"><big>Successfuly uploaded <i class="glyphicon glyphicon-thumbs-up"></i><br>  We will get back to you shortly.</div>
+
+</fieldset>
+</div>
+<script type="text/javascript">
+	     $(document).ready(function() {
+    $('#contact_form').bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        
+        })
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
+});
+
+
+
+
+</script>
+</body>
+</html>
--- Task 4 php/php/form.php
+++ Task 4 php/php/form.php
@@ -21,20 +21,28 @@
 <legend>Join Us Today!</legend>
 <div align=right>
 <a href="index.html" class=button>Home</a></div><br>
+<p align="center"><b>
+    <big>HELLO <?php echo $_GET["first_name"]; ?> <?php echo $_GET["last_name"]; ?>
+<br>Please check your details before joining</big></b></p>
+<br><br> 
 <p><b>
-First Name: <?php echo $_POST["first_name"]; ?><br><br>
-Last Name: <?php echo $_POST["last_name"]; ?><br><br>
-Gender: <?php echo $_POST["Gender"]; ?><br><br>
-Email address: <?php echo $_POST["email"]; ?><br><br>
-Phone: <?php echo $_POST["phone"]; ?><br><br>
-Address: <?php echo $_POST["address"]; ?><br><br>
-City: <?php echo $_POST["city"]; ?><br><br>
-State/Union Territory: <?php echo $_POST["state"]; ?><br><br>
-Zip Code: <?php echo $_POST["zip"]; ?><br><br>
-Website or domain name: <?php echo $_POST["website"]; ?><br><br>
+Gender: <?php echo $_GET["Gender"]; ?><br><br>
+Email address: <?php echo $_GET["email"]; ?><br><br>
+Phone: <?php echo $_GET["phone"]; ?><br><br>
+Address: <?php echo $_GET["address"]; ?><br><br>
+City: <?php echo $_GET["city"]; ?><br><br>
+State/Union Territory: <?php echo $_GET["state"]; ?><br><br>
+Zip Code: <?php echo $_GET["zip"]; ?><br><br>
+Website or domain name: <?php echo $_GET["website"]; ?><br><br>
 </b>
 </p>
-<div class="alert alert-success" role="alert" id="success_message"><big>Successfuly uploaded <i class="glyphicon glyphicon-thumbs-up"></i><br>  We will get back to you shortly.</div>
+<form class="well form-horizontal" action="data.php" method="get"  id="join_form">
+    <div class="form-group">
+  <label class="col-md-4 control-label"></label>
+  <div class="col-md-4">
+    <button type="submit" class="btn btn-warning" >Send <span class="glyphicon glyphicon-send"></span></button>
+  </div>
 </div>
+</form>
 </body>
 </html>
